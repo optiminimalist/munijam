164a731c8be8535fe0ea1438f1e5a05abe0323ff
Michael Luckeneder <michael@luckeneder.net>
2017-07-15 10:42:19 -0700

    analysis and new batch

--- fetch_muni_data.py
+++ fetch_muni_data.py
@@ -4,11 +4,13 @@ from nextbus import get_raw_muni_data
 
 if __name__ == "__main__":
     while(True):
-        response = get_raw_muni_data()
-        fname = "tmpdata/"+ str(int(time.time())) + ".xml"
-        with open(fname, 'w') as f:
-            f.write(response)
-            print("Wrote: "+fname)
-
+        try:
+            response = get_raw_muni_data()
+            fname = "tmpdata/"+ str(int(time.time())) + ".xml"
+            with open(fname, 'w') as f:
+                f.write(response)
+                print("Wrote: "+fname)
+        except:
+            print("Error")
         print("Sleeping....")
         time.sleep(300)
